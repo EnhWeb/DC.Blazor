b2ca36e33bffe14210477b8342eb96046a503f1f
丁川 <100538511@qq.com>
2019-03-22 18:32:37 +0800

    测试Http请求拦截器功能

--- samples/Blazor.Client/Startup.cs
+++ samples/Blazor.Client/Startup.cs
@@ -1,5 +1,7 @@
+using DC.HttpClientInterceptor;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Blazor.Client
 {
@@ -12,13 +14,30 @@ namespace Blazor.Client
             services.AddDCToast();  // 注册提示框
 
             services.AddDCLocalisation(); // 注册本地化
+
+            services.AddDCHttpClientInterceptor(); // 注册Http请求拦截器
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
-            app.UseDCLocalisationExtension();  //启动本地化操作
+            app.UseDCLocalisationExtension();  // 启动本地化操作
+
+            app.UseDCHttpClientInterceptor();  // 启用Http请求拦截器
+            var httpInterceptor = app.Services.GetService<HttpClientInterceptor>();
+            httpInterceptor.BeforeSend += HttpInterceptor_BeforeSend;
+            httpInterceptor.AfterSend += HttpInterceptor_AfterSend;
 
             app.AddComponent<App>("app");
+        }
+
+        private void HttpInterceptor_BeforeSend(object sender, EventArgs e)
+        {
+            Console.WriteLine("BeforeSend event of HttpClientInterceptor");
+        }
+
+        private void HttpInterceptor_AfterSend(object sender, EventArgs e)
+        {
+            Console.WriteLine("AfterSend event of HttpClientInterceptor");
         }
     }
 }
