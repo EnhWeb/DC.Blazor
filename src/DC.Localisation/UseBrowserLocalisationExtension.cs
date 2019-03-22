1f9c063f963f0fe7c584542129342d5f826282e6
丁川 <100538511@qq.com>
2019-03-22 12:30:58 +0800

    增加前端本地化代码

---
+++ src/DC.Localisation/UseBrowserLocalisationExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace Microsoft.AspNetCore.Components.Builder
+{
+    public static class UseBrowserLocalisationExtension
+    {
+        public static void UseDCLocalisationExtension(this IComponentsApplicationBuilder app)
+        {
+            var jsRuntime = app.Services.GetService(typeof(IJSRuntime));
+            var browserLocale = ((IJSInProcessRuntime)jsRuntime).Invoke<string>("DC.Localisation.GetBrowserLocale");
+            var culture = new CultureInfo(browserLocale);
+
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+        }
+    }
+}
