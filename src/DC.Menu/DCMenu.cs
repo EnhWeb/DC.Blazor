7d88966c2853b945171c1a762a62bea47b6657bc
丁川 <100538511@qq.com>
2019-03-24 12:35:56 +0800

    Blazor自定义菜单组件增加并测试其中一种方式调用

---
+++ src/DC.Menu/DCMenu.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.RenderTree;
+using System;
+
+namespace DC.Menu
+{
+    public class DCMenu : ComponentBase
+    {
+        /// <summary>
+        /// 菜单子内容
+        /// </summary>
+        [Parameter] RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// 菜单构建器
+        /// </summary>
+        [Parameter] MenuBuilder MenuBuilder { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            if (ChildContent != null && MenuBuilder != null)
+            {
+                throw new InvalidOperationException($"不能同时使用菜单子内容和菜单构建器");
+            }
+
+            if (ChildContent != null)
+            {
+                BuildMenuFromMarkup(builder);
+            }
+            else if (MenuBuilder != null)
+            {
+                BuildMenuFromBuilder(builder);
+            }
+        }
+
+        private void BuildMenuFromMarkup(RenderTreeBuilder builder)
+        {
+            var index = -1;
+
+            builder.OpenElement(index++, "nav");
+            builder.AddAttribute(index++, "role", "navigation");
+            builder.AddAttribute(index++, "class", "dc-menu-container");
+            builder.OpenElement(index++, "ul");
+            builder.AddAttribute(index++, "class", "dc-menu");
+            builder.AddContent(index++, ChildContent);
+            builder.CloseElement();
+            builder.CloseElement();
+        }
+
+        private void BuildMenuFromBuilder(RenderTreeBuilder builder)
+        {
+            var index = -1;
+            var menuItems = MenuBuilder.Build(x => x.Position);
+
+            builder.OpenElement(index++, "nav");
+            builder.AddAttribute(index++, "role", "navigation");
+            builder.AddAttribute(index++, "class", "dc-menu-container");
+            builder.OpenElement(index++, "ul");
+            builder.AddAttribute(index++, "class", "dc-menu");
+
+            foreach (var item in menuItems)
+            {
+                if (item.IsSubMenu && item.IsVisible)
+                {
+                    index = BuildSubMenu(builder, index, item);
+                }
+                else if (!item.IsSubMenu && item.IsVisible)
+                {
+                    index = BuildMenuItem(builder, index, item);
+                }
+            }
+
+            builder.CloseElement();
+            builder.CloseElement();
+        }
+
+        private int BuildSubMenu(RenderTreeBuilder subMenuBuilder, int index, MenuItem item)
+        {
+            index++;
+            return index;
+        }
+
+        private int BuildMenuItem(RenderTreeBuilder menuItemBuilder, int index, MenuItem item)
+        {
+            menuItemBuilder.OpenComponent<DCMenuItem>(index++);
+
+            if (item.IsVisible)
+            {
+
+            }
+
+            menuItemBuilder.CloseComponent();
+
+            return index;
+        }
+    }
+}
---
+++ src/DC.Menu/MenuBuilder.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DC.Menu
+{
+    /// <summary>
+    /// 菜单构建器
+    /// </summary>
+    public class MenuBuilder
+    {
+        /// <summary>
+        /// 菜单集合
+        /// </summary>
+        private List<MenuItem> _menuItems;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public MenuBuilder()
+        {
+            _menuItems = new List<MenuItem>();
+        }
+
+        public MenuBuilder AddItem(int position, string title, string link, bool IsVisible = true, bool IsEnabled = true)
+        {
+            var menuItem = new MenuItem();
+            menuItem.Position = position;
+            menuItem.Title = title;
+            menuItem.Link = link;
+            menuItem.IsSubMenu = false;
+            menuItem.IsVisible = IsVisible;
+            menuItem.IsEnabled = IsEnabled;
+
+            _menuItems.Add(menuItem);
+
+            return this;
+        }
+
+        public MenuBuilder AddSubMenu(int position, string title, MenuBuilder menuItems, bool IsVisible = true, bool IsEnabled = true)
+        {
+            var menuItem = new MenuItem();
+            menuItem.Position = position;
+            menuItem.Title = title;
+            menuItem.IsSubMenu = true;
+            menuItem.MenuItems = menuItems;
+            menuItem.IsVisible = IsVisible;
+            menuItem.IsEnabled = IsEnabled;
+
+            _menuItems.Add(menuItem);
+
+            return this;
+        }
+
+        /// <summary>
+        /// 菜单排序
+        /// </summary>
+        /// <param name="orderBy"></param>
+        /// <returns></returns>
+        internal List<MenuItem> Build(Func<MenuItem, int> orderBy)
+        {
+            var menuItems = _menuItems.OrderBy(orderBy);
+
+            return menuItems.ToList();
+        }
+
+    }
+
+    /// <summary>
+    /// 菜单项
+    /// </summary>
+    public class MenuItem
+    {
+        /// <summary>
+        /// 菜单排序
+        /// </summary>
+        public int Position { get; set; }
+
+        /// <summary>
+        /// 菜单标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 菜单链接
+        /// </summary>
+        public string Link { get; set; }
+
+        /// <summary>
+        /// 子菜单项
+        /// </summary>
+        public MenuBuilder MenuItems { get; set; }
+
+        /// <summary>
+        /// 是否上级菜单
+        /// </summary>
+        public bool IsSubMenu { get; set; }
+
+        /// <summary>
+        /// 是否可见
+        /// </summary>
+        public bool IsVisible { get; set; }
+
+        /// <summary>
+        /// 是否可用
+        /// </summary>
+        public bool IsEnabled { get; set; }
+    }
+}
